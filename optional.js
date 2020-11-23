52e9192cda4e8c344990905f1dcd1e3c456f8078
haruhiko-zht <psp.in.haruhiko@gmail.com>
2020-11-23 21:08:09 +0900

    Initial commit

---
+++ optional.js
@@ -0,0 +1,19 @@
+// Use jQuery
+
+// $(function(){
+// Scrolling top by Javascript
+// $('.sp-menu__item a').on('click', function(e){
+//   let href = ($(this).attr('href') !== undefined) ? $(this).attr('href') : null;
+//   if(href === '#scroll-top') {
+//     $('body, html').animate({
+//       scrollTop: 0
+//     }, 500);
+//     return false;
+//   }
+// });
+
+// Search form focus
+// $('#sp-menu__item-search').on('change', function(){
+//   $('input[type="text"]').focus();
+// });
+// });
